bab23b80855c0688c9639818819844f9d344043a
icarlyle <icarlyle@mail.umw.edu>
2017-04-12 04:34:26 +0000

    Made accound information look good

--- static/js/controller.js
+++ static/js/controller.js
@@ -2,21 +2,25 @@ var App = angular.module('App', []);
 
 App.controller('AppController', function($scope){
     var socket = io.connect('https://' + document.domain + ':' + location.port);
+    
+    $scope.totals = [];
 
     $scope.cartqty = function cartqty(qty, pid){
-        $scope.qty=qty
-        $scope.pid=pid
+        $scope.qty=qty;
+        $scope.pid=pid;
         socket.emit('cartqty', $scope.pid, $scope.qty);
         console.log('adjusting quantity');
     };
     
-    socket.on('adjustedqty', function(){
-        console.log('adjusted quantity');
+    socket.on('totals', function(totals){
+        $scope.totals = totals;
+        $scope.$apply();
     });
     
     
     socket.on('connect', function(){
         console.log('connected');
+        
     });
     
 });
